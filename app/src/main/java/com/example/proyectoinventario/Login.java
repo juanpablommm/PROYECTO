0a16dbfb733e36a558519408653e41f2d6870a73
Geisler <geisler@misena.edu.co>
2020-02-07 16:46:16 -0500

    realice mejorar en la interfaz de inicio agredando campos y asignando los id de cada campo

--- app/src/main/java/com/example/proyectoinventario/Login.java
+++ app/src/main/java/com/example/proyectoinventario/Login.java
@@ -3,13 +3,22 @@ package com.example.proyectoinventario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Login extends AppCompatActivity {
 
+    Spinner Opciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        Opciones=findViewById(R.id.SpinnerOpciones);
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,
+                R.array.cuentas,android.R.layout.simple_spinner_item);
+
+        Opciones.setAdapter(adapter);
     }
 }
