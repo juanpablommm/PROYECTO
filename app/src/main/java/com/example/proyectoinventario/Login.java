e05ac41bdb8cc133704b5232c2cb3e5964e604cf
@esteban216g <esteban216g@gmail.com>
2020-02-07 09:52:45 -0500

    cambio de nombre primera actividad

--- app/src/main/java/com/example/proyectoinventario/Login.java
+++ app/src/main/java/com/example/proyectoinventario/Login.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.logui);
+        setContentView(R.layout.login);
 
     }
 }
