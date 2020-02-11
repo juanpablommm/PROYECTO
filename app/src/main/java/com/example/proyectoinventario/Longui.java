5e760a16607ca1cda08c5c5b02614c8c1f324e47
@esteban216g <esteban216g@gmail.com>
2020-02-11 09:16:18 -0500

    Se corrigieron errores

---
+++ app/src/main/java/com/example/proyectoinventario/Longui.java
@@ -0,0 +1,14 @@
+package com.example.proyectoinventario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Longui extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_longui);
+    }
+}
--- app/src/main/java/com/example/proyectoinventario/Menu.java
+++ app/src/main/java/com/example/proyectoinventario/Menu.java
@@ -19,8 +19,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.Menu;
-
 public class Menu extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -53,12 +51,7 @@ public class Menu extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onSupportNavigateUp() {
