2f8e5f01b92bcd9874a015f7fd11179239bb1391
Geisler <geisler@misena.edu.co>
2020-02-16 22:09:37 -0500

    se agrego un actividad para la portada principal de nuestro menu

---
+++ app/src/main/java/com/example/proyectoinventario/PantallaInicio.java
@@ -0,0 +1,14 @@
+package com.example.proyectoinventario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio);
+    }
+}
