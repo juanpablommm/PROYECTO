e43ad86e55e50a3dd2c87ffc8d30dff520644cc4
JUAN <12345>
2020-02-17 19:28:51 -0500

    Se modifico la interface de Registrar los activos de manera q quedo en una tabla

--- app/src/main/java/com/example/proyectoinventario/ui/Registrar_Activos/Registrar_Activos.java
+++ app/src/main/java/com/example/proyectoinventario/ui/Registrar_Activos/Registrar_Activos.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -18,18 +20,38 @@ public class Registrar_Activos extends Fragment {
 
     private ShareViewModel shareViewModel;
 
+    private Spinner Spinner_Activo;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
+
+
                              ViewGroup container, Bundle savedInstanceState) {
+
+
         shareViewModel =
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.registrar_activos, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
+        Spinner_Activo=(Spinner) root.findViewById(R.id.Spinner_Activo);
+        final TextView textView = root.findViewById(R.id.Titulo_Registrar_Activos);
         shareViewModel.getText().observe(this, new Observer<String>() {
+
+
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+
+               // String [] inventario= {"Equipo","Material","Herramienta"};
+                //ArrayAdapter <String>adapter_inventario= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, inventario);
+                //Spinner_Activo.setAdapter(adapter_inventario);
+
+
             }
         });
         return root;
+
+
+
     }
+
+
 }
