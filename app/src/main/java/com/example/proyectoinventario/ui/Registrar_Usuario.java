055ea726ce7278ee0d07cd92ce265c455c9cffda
Geisler <geisler@misena.edu.co>
2020-03-05 22:34:08 -0500

    codigo

--- app/src/main/java/com/example/proyectoinventario/ui/Registrar_Usuario.java
+++ app/src/main/java/com/example/proyectoinventario/ui/Registrar_Usuario.java
@@ -1,64 +1,85 @@
 package com.example.proyectoinventario.ui;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.proyectoinventario.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 public class Registrar_Usuario extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        private FirebaseAuth mAuth;
+         EditText editTextNombre;
+         EditText editTextApellido;
+         EditText editTextCedula;
+         EditText editTextTelefono;
+         EditText editTextCorreo;
+         EditText editTextContraseña;
+         EditText editTextRol;
 
-    public Registrar_Usuario() {
-        // Required empty public constructor
-    }
+       @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fragment_registrar__usuario);
+            mAuth = FirebaseAuth.getInstance();
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Registrar_Usuario.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Registrar_Usuario newInstance(String param1, String param2) {
-        Registrar_Usuario fragment = new Registrar_Usuario();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+            editTextNombre.findViewById(R.id.Nombre_usu);
+            editTextApellido.findViewById(R.id.Apellido_usu);
+            editTextCedula.findViewById(R.id.Documento_usu);
+            editTextTelefono.findViewById(R.id.Telefono_usu);
+            editTextCorreo.findViewById(R.id.In_correo_usu);
+            editTextContraseña.findViewById(R.id.In_Contraseña_usu);
+            editTextRol.findViewById(R.id.In_rol_usu);
+
+
+        }
+
+    private void setContentView(int fragment_registrar__usuario) {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        public void onStart() {
+            super.onStart();
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        }
+
+        public void saveUser(View view) {
+
+            String email = editTextCorreo.getText().toString();
+            String password = editTextContraseña.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+
+
+                            }
+
+                        }
+                    });
+
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registrar__usuario, container, false);
-    }
 
 
-}
