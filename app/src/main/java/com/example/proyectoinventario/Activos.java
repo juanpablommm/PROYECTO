458cdf34957dcd2de1aa88d36948bd19bcdd8ad8
JUAN <12345>
2020-03-07 19:39:20 -0500

    Se agrego la clase jav para el registro de usuario, lo mismo que el codigo necesario para la creacion del nodo activos en la base de datos firebase

---
+++ app/src/main/java/com/example/proyectoinventario/Activos.java
@@ -0,0 +1,36 @@
+package com.example.proyectoinventario;
+
+public class Activos {
+
+    String Idactivo,NombreActivo, CantidadActivo, MarcaActivo, SerialActivo;
+
+
+    public Activos(String idactivo, String nombreActivo, String cantidadActivo, String marcaActivo, String serialActivo) {
+        Idactivo = idactivo;
+        NombreActivo = nombreActivo;
+        CantidadActivo = cantidadActivo;
+        MarcaActivo = marcaActivo;
+        SerialActivo = serialActivo;
+    }
+
+
+    public String getIdactivo() {
+        return Idactivo;
+    }
+
+    public String getNombreActivo() {
+        return NombreActivo;
+    }
+
+    public String getCantidadActivo() {
+        return CantidadActivo;
+    }
+
+    public String getMarcaActivo() {
+        return MarcaActivo;
+    }
+
+    public String getSerialActivo() {
+        return SerialActivo;
+    }
+}
--- app/src/main/java/com/example/proyectoinventario/ui/RegistrarActivos/RegistrarActivos.java
+++ app/src/main/java/com/example/proyectoinventario/ui/RegistrarActivos/RegistrarActivos.java
@@ -1,12 +1,16 @@
 package com.example.proyectoinventario.ui.RegistrarActivos;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -14,29 +18,63 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.proyectoinventario.Activos;
 import com.example.proyectoinventario.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrarActivos extends Fragment {
+
+    EditText Nombre_activo;
+    EditText Cantidad_activo;
+    EditText Marca_activo;
+    EditText Serial_activo;
+
 
     private ShareViewModel shareViewModel;
 
     private Spinner Spinner_Activo;
+    Button buttonRegistrar;
+    private DatabaseReference Activosdb;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
 
-
                              ViewGroup container, Bundle savedInstanceState) {
-
+       // Activosdb= FirebaseDatabase.getInstance().getReference("Activos");
 
         shareViewModel =
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.registrar_activos, container, false);
+
+        Nombre_activo=(EditText)root.findViewById(R.id.In_Nombre_Activo);
+        Cantidad_activo=(EditText)root.findViewById(R.id.In_Cantidad_Activo);
+        Marca_activo=(EditText)root.findViewById(R.id.In_Marca_Activo);
+        Serial_activo=(EditText)root.findViewById(R.id.In_Serial_Activo);
         Spinner_Activo=(Spinner) root.findViewById(R.id.Spinner_Activo);
+        buttonRegistrar=(Button) root.findViewById(R.id.button_Registrar_Activo);
         final TextView textView = root.findViewById(R.id.RegistrarActivos);
 
         return root;
 
 
+
+    }
+
+    public void registraractivos(){
+        String NombreActivo=Nombre_activo.getText().toString();
+        String CantidadActivo=Cantidad_activo.getText().toString();
+        String MarcaActivo=Marca_activo.getText().toString();
+        String SerialActivo=Serial_activo.getText().toString();
+
+        if (!TextUtils.isEmpty(NombreActivo)){
+            String id= Activosdb.push().getKey();
+            Activos activos=new Activos(id,NombreActivo, CantidadActivo, MarcaActivo, SerialActivo);
+            Activosdb.child("activos").child(id).setValue(activos);
+            Toast.makeText(getContext(),"Regisstro Exitoso",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getContext(),"ingresar datos",Toast.LENGTH_SHORT).show();
+        }
 
     }
 
