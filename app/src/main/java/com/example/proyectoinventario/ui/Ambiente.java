3038e8dca8831515dfe7875ff49dc4951fc39732
JUAN <12345>
2020-03-13 15:26:30 -0500

    Se creo el codigo correspondiente y la clase java para para la insercion del nodo ambien en firebase

---
+++ app/src/main/java/com/example/proyectoinventario/ui/Ambiente.java
@@ -0,0 +1,33 @@
+package com.example.proyectoinventario.ui;
+
+public class Ambiente {
+
+    private String idAmbienete;
+    private String Nombre_Ambiente;
+    private String Codigo_Ambiente;
+    private String Zona_Ambiente;
+
+
+    public Ambiente(String idAmbienete, String nombre_Ambiente, String codigo_Ambiente, String zona_Ambiente) {
+        this.idAmbienete = idAmbienete;
+        Nombre_Ambiente = nombre_Ambiente;
+        Codigo_Ambiente = codigo_Ambiente;
+        Zona_Ambiente = zona_Ambiente;
+    }
+
+    public String getIdAmbienete() {
+        return idAmbienete;
+    }
+
+    public String getNombre_Ambiente() {
+        return Nombre_Ambiente;
+    }
+
+    public String getCodigo_Ambiente() {
+        return Codigo_Ambiente;
+    }
+
+    public String getZona_Ambiente() {
+        return Zona_Ambiente;
+    }
+}
--- app/src/main/java/com/example/proyectoinventario/ui/RegistrarAmbiente/RegistrarAmbiente.java
+++ app/src/main/java/com/example/proyectoinventario/ui/RegistrarAmbiente/RegistrarAmbiente.java
@@ -1,10 +1,15 @@
 package com.example.proyectoinventario.ui.RegistrarAmbiente;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -12,22 +17,63 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.proyectoinventario.Activos;
 import com.example.proyectoinventario.R;
+import com.example.proyectoinventario.Recuperar_Contrasenia;
+import com.example.proyectoinventario.ui.Ambiente;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class RegistrarAmbiente extends Fragment {
+
+
+
+    private EditText editTextNombre_Ambiente;
+    private EditText editTextCodigo_Ambiente;
+    private EditText editTextZona_Abiemte;
+
+    private Button botton_Registrar_Ambiente;
+    private DatabaseReference Ambientedb;
+
 
     private SlideshowViewModel slideshowViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        Ambientedb= FirebaseDatabase.getInstance().getReference("Ambiente");
+
+
+
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.registrar_ambiente, container, false);
         final TextView textView = root.findViewById(R.id.Registrar_Ambiente);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
+        editTextNombre_Ambiente=(EditText)root.findViewById(R.id.fragment_Nombre_Ambiente);
+        editTextCodigo_Ambiente=(EditText)root.findViewById(R.id.fragment_Codigo_Ambiente);
+        editTextZona_Abiemte=(EditText)root.findViewById(R.id.fragment_Zona_Ambiente);
+        botton_Registrar_Ambiente=(Button) root.findViewById(R.id.fragment_boton_eliminar);
+
+        botton_Registrar_Ambiente.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+
+                String Nombre_Ambiente=editTextNombre_Ambiente.getText().toString();
+                String Codigo_Ambiente=editTextCodigo_Ambiente.getText().toString();
+                String Zona_Ambiente=editTextZona_Abiemte.getText().toString();
+
+                if (!TextUtils.isEmpty(Nombre_Ambiente)){
+                    String id= UUID.randomUUID().toString();
+                    Ambiente ambiente =new Ambiente(id,Nombre_Ambiente,Codigo_Ambiente,Zona_Ambiente);
+                    Ambientedb.child(id).setValue(ambiente);
+                    Toast.makeText(getContext(),"Regisstro Exitoso",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getContext(),"ingresar datos",Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
         return root;
